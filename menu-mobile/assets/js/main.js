2a53e55e84ce95fef57a9af2944b7b1822fd2360
thuan.nv <nguyenthuankma@gmail.com>
2023-08-05 14:47:59 +0700

    Update code fixbug popup web36

--- menu-mobile/assets/js/main.js
+++ menu-mobile/assets/js/main.js
@@ -2,15 +2,16 @@ const ic_menu = document.querySelector('.menu-mobile');
 const popupMenu = document.querySelector('.poup-menu-mobile');
 const bgMenu = document.querySelector('.bg-menu-mobile');
 
-let isClick = false;
+let isPopupOpen = false;
 
 
-ic_menu.addEventListener('click', ()=> {
-  // ic_menu.style.le
+ic_menu.addEventListener('click', () => {
   bgMenu.style.left = "0px";
-
+  isPopupOpen = true;
 })
 
-bgMenu.addEventListener('click', ()=> {
-  console.log(bgMenu.classList.contains('poup-menu-mobile'));
+bgMenu.addEventListener('click', (event) => {
+  if(event.target.classList.contains('bg-menu-mobile') === true ) {
+    bgMenu.style.left = "-500%";
+  }
 })
